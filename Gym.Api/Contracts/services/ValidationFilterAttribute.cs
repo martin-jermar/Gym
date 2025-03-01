6903b1553e86b49234df1b7e8466bc17ca63a866
martin <m.jermar@seznam.cz>
2025-03-01 21:48:07 +0100

    ValidationFilterAttribute

--- Gym.Api/Contracts/services/ValidationFilterAttribute.cs
+++ Gym.Api/Contracts/services/ValidationFilterAttribute.cs
@@ -2,24 +2,23 @@ namespace Gym.Api.Contracts.Services;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-public class ValidationFilter : IResultFilter
+public abstract class ValidationFilterAttribute : IResultFilter
 {
     public void OnResultExecuting(ResultExecutingContext context)
     {
         if (!context.ModelState.IsValid)
         {
             var errors = context.ModelState
-                .Where(x => x.Value.Errors.Count > 0)
+                .Where(x => x.Value!.Errors.Count > 0)
                 .ToDictionary(
                     kvp => kvp.Key,
-                    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+                    kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray());
 
             var response = new ValidationProblemDetails
             {
                 Title = "Validation Failed",
-                
+
                 Status = StatusCodes.Status403Forbidden,
                 Errors = errors,
             };
